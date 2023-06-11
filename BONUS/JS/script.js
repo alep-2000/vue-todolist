9643fe9732b6867c5a17aae0fcef344e75d55202
alep-2000 <alessiopiras056@gmail.com>
2023-06-11 15:57:36 +0200

    File readme function modified task

---
+++ BONUS/JS/script.js
@@ -0,0 +1,67 @@
+"use strict"
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            title_app: 'VUE TO DO LIST',
+            title_task: 'To Do List',
+            tasks: [
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Comprare la tv',
+                    done: false
+                },
+                {
+                    text: 'Fare l\'esercizio',
+                    done: true
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: false
+                },
+                {
+                    text: 'Appendere la tenda',
+                    done: true
+                },
+                {
+                    text: 'Ascoltare la musica',
+                    done: false
+                },
+                {
+                    text: 'Suonare la batteria',
+                    done: true
+                },
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                },
+                {
+                    text: 'Mettere il diesel',
+                    done: false
+                }
+            ],
+            new_task: ''
+        }
+    },
+    methods: {
+        change(index){
+            this.tasks[index].done = !this.tasks[index].done
+        },
+        remove(index){
+            this.tasks.splice(index, 1)
+        },
+        add_task(){
+            let obj ={
+                        text: this.new_task,
+                        done: false
+                    }
+            this.tasks.push(obj);
+            this.new_task = ''
+        }
+    },
+}).mount('#app');
---
+++ BONUS/readme.md
@@ -0,0 +1,7 @@
+PROBLEMA:
+Aggiungete la funzionalità di edit della task
+
+SOLUZIONE:
+1 - Aggiungere nel markup statico un button per la modifica;
+2 - Aggiungere un evento al click con collegata una funzione;
+3 - Creare una funzione che permetta di modificare la task;
